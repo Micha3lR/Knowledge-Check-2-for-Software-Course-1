42d9dc8bbf5ec72e638faf8d0ffda6279445584a
Michael Evans <167821301+Micha3lR@users.noreply.github.com>
2024-09-14 17:16:20 -0400

    Knowledge Check Complete

---
+++ KnowledgeCheck2/Fruit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KnowledgeCheck2
+{
+    internal class Fruit
+    {
+        public String Name { get; set; }
+        public double WeightInPounds { get; set; }
+
+
+        public override string ToString()
+        {
+            return $"Name: {Name}\nWeight: {WeightInPounds} lbs\n";
+        }
+    }
+}
---
+++ KnowledgeCheck2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace KnowledgeCheck2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many records do you want to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
+
+            var recordList = new List<Apple>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var apple = new Apple();
+
+                Console.WriteLine("Enter the value for (Price): ");
+                apple.Price = decimal.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the value for (Name): ");
+                apple.Name = Console.ReadLine();
+
+                Console.WriteLine("Enter the value for (Weight): ");
+                apple.WeightInPounds = double.Parse(Console.ReadLine());
+
+                recordList.Add(apple);
+            }
+
+            // Print out the list of records using Console.WriteLine()
+            Console.WriteLine("\nApple records:");
+            foreach (var apple in recordList)
+            {
+                Console.WriteLine(apple);
+            }
+        }
+    }
+}
